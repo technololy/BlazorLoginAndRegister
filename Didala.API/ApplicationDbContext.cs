7874db144fa28fdfce8a5f93f9790fb3208fc9f3
ololade ahmed oyebanji <loladeking@hotmail.com>
2020-08-29 04:43:18 +0100

    first commit

---
+++ Didala.API/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Didala.API
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Models.ApplicationUser> userManager { get; set; }
+
+    }
+}
---
+++ Didala.API/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace Didala.API.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser()
+        {
+        }
+    }
+}
